1e1d5c79ffb8a8809a36f5dc800da8ecffde30e6
Marta Kasprzyk <martha.kasprzyk@gmail.com>
2024-10-08 14:06:02 +0200

    added implementation of methods nextProgram and previousProgram and created getIndex method

--- src/main/java/com/javaexam/washingmachine/WashingMachine.java
+++ src/main/java/com/javaexam/washingmachine/WashingMachine.java
@@ -24,6 +24,65 @@ public class WashingMachine implements Program {
         }
     }
 
+    @Override
+    public int nextProgram() {
+        int nextProgram = 0;
+        int currentProgram = getProgram();
+        boolean programAllowed = checkIfProgramAllowed(currentProgram);
+
+        if (programAllowed) {
+            int index = getIndex(currentProgram);
+
+            if (currentProgram == 10) {
+                nextProgram = availableProgramsValues.get(0).getAvailableProgram();
+            } else {
+                if (currentProgram >= 1 && currentProgram < 10) {
+                    nextProgram = availableProgramsValues.get(index + 1).getAvailableProgram();
+                } else {
+                    System.out.println("Sth went wrong");
+                }
+            }
+        } else {
+            nextProgram = -1;
+        }
+        return nextProgram;
+    }
+
+    @Override
+    public int previousProgram() {
+        int previousProgram = 0;
+        int currentProgram = getProgram();
+        boolean programAllowed = checkIfProgramAllowed(currentProgram);
+
+        if (programAllowed) {
+            int index = getIndex(currentProgram);
+
+            if (currentProgram == 1) {
+                previousProgram = availableProgramsValues.get(9).getAvailableProgram();
+            } else {
+                if (currentProgram > 1 && currentProgram <= 10) {
+                    previousProgram = availableProgramsValues.get(index - 1).getAvailableProgram();
+                } else {
+                    System.out.println("Sth went wrong");
+                }
+            }
+        } else {
+            previousProgram = -1;
+        }
+        return previousProgram;
+    }
+
+    private int getIndex(int programNumber) {
+        int index = -1;
+        for (int i = 0; i < availableProgramsValues.size(); i++) {
+            if (availableProgramsValues.get(i).getAvailableProgram() == programNumber) {
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+
     private boolean checkIfProgramAllowed(int programNumber) {
         for (AvailablePrograms program : AvailablePrograms.values()) {
             if (program.getAvailableProgram() == programNumber) {
