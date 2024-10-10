66e04db403000040c115d76faf83b853cfa33c8f
Marta Kasprzyk <martha.kasprzyk@gmail.com>
2024-10-10 11:46:24 +0200

    Main update - called methods from WashingMachine

--- src/main/java/com/javaexam/washingmachine/Main.java
+++ src/main/java/com/javaexam/washingmachine/Main.java
@@ -1,8 +1,67 @@
 package com.javaexam.washingmachine;
 
-public class Main {
-    public static void main(String[] args) {
+import com.javaexam.washingmachine.domain.LaundryProgram;
+import com.javaexam.washingmachine.domain.WashingMachine;
+import com.javaexam.washingmachine.exception.*;
+import com.javaexam.washingmachine.service.LaudryProgramGenerator;
 
-        }
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws SpeedOutOfRangeException, TemperatureUnitNotValidException, TemperatureOutOfRangeException, InvalidDelayValueException, InvalidProgramException {
+
+        // generate list of laundry programs
+        LaudryProgramGenerator generator = new LaudryProgramGenerator();
+        List<LaundryProgram> programs = generator.createLaundryPrograms(10);
+
+        // create new Washing Machine
+        WashingMachine machine = new WashingMachine(programs);
+//        machine.showStatus();
+//        machine.showHistory();
+
+        // set new Laundry
+        machine.setNewLaundry();
+        machine.showStatus();
+
+        // modify Laundry
+//        machine.tempUp();
+//        machine.showStatus();
+
+//        machine.tempDown();
+//        machine.showStatus();
+
+//        machine.convertTemp();
+//        machine.showStatus();
+
+//        machine.speedUp();
+//        machine.showStatus();
+
+//        machine.speedDown();
+//        machine.showStatus();
+//
+//        machine.speedUp();
+//        machine.showStatus();
+
+//        // change program
+//        machine.nextProgram();
+//        machine.showStatus();
+
+//        machine.previousProgram();
+//        machine.showStatus();
+
+//        machine.setProgram(programs.get(3));
+//        machine.showStatus();
+
+//        machine.setLaundryDelay(15);
+//        machine.showStatus();
+
+//        // do laundry -> save to history
+//        machine.doLaundry();
+//        machine.showHistory();
+//        machine.showStatus();
+//
+//        // check list of all programs for WM
+//        System.out.println(machine.getPrograms());
+
     }
 }
